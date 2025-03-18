5eadf30de606c2f4bd92385986bb9d4c0fd9ec7a
nemoymoy <nemoymoy@gmail.com>
2025-03-18 11:11:21 +0400

    В классе "Mentor" атрибут "courses_attached" сделан обязательным для всех экземпляров класса. Создан класс "Lecturer" и "Reviewer".

--- DZ_OOP_1.py
+++ DZ_OOP_1.py
@@ -12,20 +12,20 @@ class Student:
         self.finished_courses.append(course_name)
 
 
-class Mentor:
-    def __init__(self, name, surname):
-        self.name = name
-        self.surname = surname
-        self.courses_attached = []
-
-    def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
-            else:
-                student.grades[course] = [grade]
-        else:
-            return 'Ошибка'
+# class Mentor:
+#     def __init__(self, name, surname):
+#         self.name = name
+#         self.surname = surname
+#         self.courses_attached = []
+#
+#     def rate_hw(self, student, course, grade):
+#         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+#             if course in student.grades:
+#                 student.grades[course] += [grade]
+#             else:
+#                 student.grades[course] = [grade]
+#         else:
+#             return 'Ошибка'
 
 # best_student = Student('Ruoy', 'Eman', 'your_gender')
 # best_student.finished_courses += ['Git']
@@ -41,18 +41,44 @@ class Mentor:
 # cool_mentor.courses_attached += ['Python']
 # print(cool_mentor.courses_attached)
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
-
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
-
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-
-print(best_student.grades)
+# best_student = Student('Ruoy', 'Eman', 'your_gender')
+# best_student.courses_in_progress += ['Python']
+#
+# cool_mentor = Mentor('Some', 'Buddy')
+# cool_mentor.courses_attached += ['Python']
+#
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+#
+# print(best_student.grades)
 
 # Код для проверки Домашнее задание «ООП: наследование, инкапсуляция и полиморфизм».
 # Задание № 1. Наследование
 
+class Mentor:
+    def __init__(self, name, surname, courses_attached):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = courses_attached
+
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+# Создание класса Лекторов
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname, courses_attached):
+        super().__init__(name, surname, courses_attached)
+
+# Создание класса Экспертов
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname, courses_attached):
+        super().__init__(name, surname, courses_attached)
